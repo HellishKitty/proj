0b07d04457413449f3c2a5cbbb6138e7693689e4
Viacheslav Germonenko <vyacheslav.germonenko@gmail.com>
2018-12-27 17:42:43 +0300

    item class improvements
    - rank field added
    - gear field added
    - rank map added

--- items/items_abstract.h
+++ items/items_abstract.h
@@ -1,14 +1,20 @@
 #ifndef _ITEMS_ABSTRACT_H
 #define _ITEMS_ABSTRACT_H
 
+#include <map>
 #include <string>
 #include <ostream>
+#define MAX_GEAR 10
 
+
+extern std::map <int, std::string>rank_map;
 
 class item {
 
 protected:
 
+    int rank;
+    int gear;
     std::string name;
     std::string type;
 
@@ -17,6 +23,8 @@ public:
     virtual ~item() = default;
     virtual int gear_up() = default;
 
+    int get_rank() { return this->rank; }
+    int get_gear() { return this->gear; }
     std::string get_name() { return this->name; }
     std::string get_type() { return this->type; }
 
