a48e67a1cd204f01f5a7c8b9d517a61f1b7ecc46
Viacheslav Germonenko <vyacheslav.germonenko@gmail.com>
2018-12-28 22:30:11 +0300

    accessory class adjust
    - now accessory class doesn't have augmentation field

--- items/accessory/accessory_abstract.h
+++ items/accessory/accessory_abstract.h
@@ -3,23 +3,13 @@
 
 #include <ostream>
 #include "../items_abstract.h"
-#define MAX_AUGMENTATION 5
 
 
 class accessory : public item {
 
-    int augmentation;
-    double pvp_attack_bonus;
-    double  pvp_defence_bonus;
-
 public:
 
     ~accessory() override = default;
-    virtual int augment_up() = 0;
-
-    int get_augmentation() { return this->augmentation; }
-    double get_attack_bonus() { return this->pvp_attack_bonus; }
-    double get_defence_bonus() { return this->pvp_defence_bonus; }
 
     friend std::ostream & operator << (std::ostream & stream, accessory & out) {
         return stream;
